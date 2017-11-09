be00bd52c86f8d5a319a8cc994ed154b42f35ddf
Tim Jones <tdjones74021@yahoo.com>
2017-11-09 19:57:10 +0000

    Create new type 'deck' which is "slice" of strings and a "receiver" function for a 'deck' to iterate through the deck of cards and print each card (with index).

--- deck.go
+++ deck.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+)
+
+// Create a new type of 'deck'
+// which is a "slice" of strings
+type deck []string
+
+// In this function 'd' is called a "receiver".
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
