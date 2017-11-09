6d8166410dc20baa1f443c7944b7b1be5f08958f
Tim Jones <tdjones74021@yahoo.com>
2017-11-09 20:16:11 +0000

    Add and implement 'newDeck' method to build up a deck of cards from the suits and values.

--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
 func main() {
-	// 'cards' is a "slice" (variable-length array)
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+
+	cards := newDeck()
 
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
